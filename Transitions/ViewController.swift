cf378c76ef9b52fada6adb1c51915cfd1ff5cbb7
chrsfabian17 <chrsfabian17@gmail.com>
2016-10-03 09:31:52 -0600

    Created a new page and a new button

--- Transitions/ViewController.swift
+++ Transitions/ViewController.swift
@@ -20,8 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func switchScreenToUpdated(sender: UIButton) {
-    }
+        @IBAction func switchScreentoUpdated(sender: UIButton)
+     {
+        
     performSegueWithIdentifier("toUpdatedFromMain", sender:sender)
     }
+}
 
--- Transitions/untitled folder/UpdatedViewControllerFile.swift
+++ Transitions/untitled folder/UpdatedViewControllerFile.swift
@@ -6,15 +6,19 @@
 //  Copyright © 2016 CTECH SWIFT. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class UpdatedViewController : UIViewController
 {
+    @IBOutlet private weak var topImage: UIImageView!
+    @IBOutlet private weak var bottomImage: UIImageView!
     
     override func viewDidLoad()
-        {
+    
+    {
            //Load the parent version of a view.
             super.viewDidLoad()
             //Load this pages components
+        bottomImage.image = UIImage(named: "IMG_0456")
     }
 }
