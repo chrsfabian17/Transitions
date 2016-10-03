8689dfd7156926ad63ea85f44dc4196588ed899f
chrsfabian17 <chrsfabian17@gmail.com>
2016-10-03 07:57:18 -0600

    Linked the added View to int main storyboard
    I created an updatedViewController

---
+++ Transitions/untitled folder/UpdatedViewControllerFile.swift
@@ -0,0 +1,20 @@
+//
+//  UpdatedViewControllerFile.swift
+//  Transitions
+//
+//  Created by Fabian, Christopher on 10/3/16.
+//  Copyright © 2016 CTECH SWIFT. All rights reserved.
+//
+
+import Foundation
+
+class UpdatedViewController : UIViewController
+{
+    
+    override func viewDidLoad()
+        {
+           //Load the parent version of a view.
+            super.viewDidLoad()
+            //Load this pages components
+    }
+}
